1300ca66b5ff7ede964810402c98890700ed1684
ilay yaacov <ilayyaacov123@gmail.com>
2025-03-06 18:30:21 +0200

    maybe could be possibly did vesa mode change

--- bl.s
+++ bl.s
@@ -36,6 +36,103 @@ main:
         mov di, 0xffff
         mov es, di
         mov bx, 0x0010
+        # graphics vesa start here
+        mov [.width], ax
+        mov [.height], bx
+        mov [.bpp], cl
+
+        sti
+
+        push es					; some VESA BIOSes destroy ES, or so I read
+        mov ax, 0x4F00				; get VBE BIOS info
+        mov di, vbe_info_block
+        int 0x10
+        pop es
+
+        cmp ax, 0x4F				; BIOS doesn't support VBE?
+        jne .error
+
+        mov ax, word[vbe_info_block.video_modes]
+        mov [.offset], ax
+        mov ax, word[vbe_info_block.video_modes+2]
+        mov [.segment], ax
+
+        mov ax, [.segment]
+        mov fs, ax
+        mov si, [.offset]
+
+        mov dx, [fs:si]
+        add si, 2
+        mov [.offset], si
+        mov [.mode], dx
+        mov ax, 0
+        mov fs, ax
+
+        cmp [.mode], 0xFFFF			; end of list?
+        je .error
+
+        push es
+        mov ax, 0x4F01				; get VBE mode info
+        mov cx, [.mode]
+        mov di, mode_info_block
+        int 0x10
+        pop es
+
+        cmp ax, 0x4F
+        jne .error
+
+        mov ax, [.width]
+        cmp ax, [mode_info_block.width]
+        jne .next_mode
+
+        mov ax, [.height]
+        cmp ax, [mode_info_block.height]
+        jne .next_mode
+
+        mov al, [.bpp]
+        cmp al, [mode_info_block.bpp]
+        jne .next_mode
+
+        mov ax, [.width]
+        mov word[vbe_screen.width], ax
+        mov ax, [.height]
+        mov word[vbe_screen.height], ax
+        mov eax, [mode_info_block.framebuffer]
+        mov dword[vbe_screen.physical_buffer], eax
+        mov ax, [mode_info_block.pitch]
+        mov word[vbe_screen.bytes_per_line], ax
+        mov eax, 0
+        mov al, [.bpp]
+        mov byte[vbe_screen.bpp], al
+        shr eax, 3
+        mov dword[vbe_screen.bytes_per_pixel], eax
+
+        mov ax, [.width]
+        shr ax, 3
+        dec ax
+        mov word[vbe_screen.x_cur_max], ax
+
+        mov ax, [.height]
+        shr ax, 4
+        dec ax
+        mov word[vbe_screen.y_cur_max], ax
+
+        ; Set the mode
+        push es
+        mov ax, 0x4F02
+        mov bx, [.mode]
+        or bx, 0x4000			; enable LFB
+        mov di, 0			; not sure if some BIOSes need this... anyway it doesn't hurt
+        int 0x10
+        pop es
+
+        cmp ax, 0x4F
+        jne .error
+
+        clc
+        ret
+
+        # ends here
         int 0x13
         jmp check_a20
 after_a20_valid:
@@ -129,7 +226,25 @@ gdt_pointer:
     dw gdt_end - gdt_start - 1  
     dd gdt_start    
     dw 0 
-          
+ # graphics   
+.next_mode:
+	mov ax, [.segment]
+	mov fs, ax
+	mov si, [.offset]
+	jmp .find_mode
+
+.error:
+	stc
+	ret
+
+.width				dw 0
+.height				dw 0
+.bpp				db 0
+.segment			dw 0
+.offset				dw 0
+.mode				dw 0
+
+# ends
 .partition_table:
     ; First partition entry (16 bytes)
     times 446 - ($-$$) db 0
